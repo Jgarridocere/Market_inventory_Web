054b5cc5ea3f4ca5ac2c09388833a74d53ff0cbb
Jgarridocere <jgarrido.cere@gmail.com>
2026-03-27 08:29:19 -0600

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,18 +1,18 @@
 <?php
 require 'includes/database.php';
 
-// Atrapamos el ID que viene en la URL
+
 $id = $_GET['id'];
 
-// Verificamos que realmente haya un ID antes de intentar borrar
+
 if ($id) {
-    // Instrucción SQL para borrar el registro específico
+    
     $query = "DELETE FROM productos WHERE id = $id";
     
-    // Ejecutamos la petición en la base de datos
+    
     $resultado = mysqli_query($conexion, $query);
 
-    // Si tuvo éxito, redireccionamos a la pantalla principal
+   
     if ($resultado) {
         header('Location: index.php');
         exit;
